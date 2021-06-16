7167af19713e94ebca4e90bccb1fffe92124d47e
yash <yashlodhiya25@gmail.com>
2021-06-16 12:05:53 +0530

    changed background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react"
 import image from "../73357.jpg"
 export default function Home() {
   return (
-    <main className="h-full flex items-center px-6 lg:px-32 bg-purple-900">
+    <main className="h-full flex items-center px-6 lg:px-32 home1">
       <img src={image} alt="Vapor" className="absolute object-cover lg:w-full lg:h-full"/>
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="libre text-6xl text-green-100 font-bold leading-none lf:leading-snug home-name"> Welcome! </h1>
