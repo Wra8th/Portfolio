55f71dba9ba4660b65e7e56234814656ad972298
yash <yashlodhiya25@gmail.com>
2021-06-16 12:14:18 +0530

    minimised icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(){
     <header className="yellowish">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
-          <NavLink to='/' exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-blue-700 text-2xl font-bold tracking-widest">
+          <NavLink to='/' exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-blue-700 font-bold tracking-widest">
             Yash
           </NavLink>
           <NavLink to='/post' activeClassName="text-pink-500  " className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 bold hover:text-blue-700 ">
@@ -21,9 +21,9 @@ export default function Navbar(){
           </NavLink>
         </nav>
         <div className="inline-flex py-3 px-3 my-6">
-          <SocialIcon url="https://github.com/Wra8th" className="mr-4" target="_blank" fgColor="#fff" style={{height:35 , width:35}}/>
-          <SocialIcon url="https://www.instagram.com/lodhiya_yash/" className="mr-4" target="_blank" fgColor="#fff" style={{height:35 , width:35}}/>
-          <SocialIcon url="https://www.linkedin.com/in/yash-lodhiya-817354192/" className="mr-4" target="_blank" fgColor="#fff" style={{height:35 , width:35}}/>
+          <SocialIcon url="https://github.com/Wra8th" className="mr-4" target="_blank" fgColor="#fff" style={{height:25 , width:25}}/>
+          <SocialIcon url="https://www.instagram.com/lodhiya_yash/" className="mr-4" target="_blank" fgColor="#fff" style={{height:25 , width:25}}/>
+          <SocialIcon url="https://www.linkedin.com/in/yash-lodhiya-817254192/" className="mr-4" target="_blank" fgColor="#fff" style={{height:25 , width:25}}/>
         </div>  
       </div>
     </header>
